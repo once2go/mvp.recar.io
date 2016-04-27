dd6abd157d5f135560835b6dff4a9f2af87e66ce
once2go <once2go@ukr.net>
2016-04-28 00:33:51 +0300

    Init MVP RX app version

---
+++ RxAppVersion/presentation/src/main/java/io/recar/denyskarpov/carpcsystem/InitSystemActivity.java
@@ -0,0 +1,13 @@
+package io.recar.denyskarpov.carpcsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitSystemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init_system);
+    }
+}
